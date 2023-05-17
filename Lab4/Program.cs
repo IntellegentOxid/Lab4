3648ac2039c8f1bf7ec82f88905f47da268d45ae
IntellegentOxid <IntellegentOxid@DESKTOP-T8IHH3H>
2023-05-17 12:50:28 +0200

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Linq;
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        // введення трьох слів 
+
+        Console.Write("Введіть перше слово: ");
+
+        string word1 = Console.ReadLine().ToLower();
+
+        Console.Write("Введіть друге слово: ");
+
+        string word2 = Console.ReadLine().ToLower();
+
+        Console.Write("Введіть третє слово: ");
+
+        string word3 = Console.ReadLine().ToLower();
+
+
+
+        // обчислення літер, які зустрічаються у всіх словах лише один раз 
+
+        var uniqueLetters = word1
+
+            .Intersect(word2)
+
+            .Intersect(word3)
+
+            .Where(c => word1.Count(x => x == c) == 1
+
+                     && word2.Count(x => x == c) == 1
+
+                     && word3.Count(x => x == c) == 1);
+
+
+
+        // виведення результату 
+
+        Console.WriteLine("Літери, які зустрічаються у всіх словах лише один раз:");
+
+        foreach (var c in uniqueLetters)
+        {
+
+            Console.Write(c + " ");
+
+        }
+
+    }
+
+}
+
